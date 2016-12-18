48a32ee966ffee5971f1f5dcffa4c67915db8c63
赤地岳 <akatigaku@gmail.com>
2016-12-18 15:30:02 +0900

    initial commit

---
+++ src/js/components/Layout.js
@@ -0,0 +1,25 @@
+import React from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+
+export default class Layout extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      title: "Welcome",
+    };
+  }
+
+  changeTitle(title) {
+    this.setState({title});
+  }
+  render() {
+    return (
+      <div>
+        <Header changeTitle={this.changeTitle.bind(this)} title={this.state.title} />
+        <Header title={"Other Title"} />
+        <Footer />
+      </div>
+    );
+  }
+}
